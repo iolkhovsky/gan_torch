images normalization bug has beed fixed

dataset/utils.py
@@ -13,37 +13,27 @@ def array_cyx2yxc(arr):
     return arr
 
 
-def normalize_cv_img(cv_img, mean=(0.5, 0.5, 0.5), std=(1.0, 1.0, 1.0)):
-    r_mean, g_mean, b_mean = mean
-    r_std, g_std, b_std = std
+def normalize_cv_img(cv_img):
     img = cv_img.astype(np.float32)
-    img = cv_img / 255.
-    out = np.zeros(shape=img.shape, dtype=np.float32)
-    out[:, :, 0] = (img[:, :, 0] - b_mean) / b_std
-    out[:, :, 1] = (img[:, :, 1] - g_mean) / g_std
-    out[:, :, 2] = (img[:, :, 2] - r_mean) / r_std
-    return out
+    img = img - 0.5 * 255.
+    img = img * (2. / 255.)
+    return img
 
 
-def denormalize_cv_array(cv_arr, mean=(0.5, 0.5, 0.5), std=(1.0, 1.0, 1.0)):
-    r_mean, g_mean, b_mean = mean
-    r_std, g_std, b_std = std
-    out = np.zeros(shape=cv_arr.shape, dtype=np.float32)
-    out[:, :, 0] = cv_arr[:, :, 0] * b_std + b_mean
-    out[:, :, 1] = cv_arr[:, :, 1] * g_std + g_mean
-    out[:, :, 2] = cv_arr[:, :, 2] * r_std + r_mean
-    out = out * 255.
-    return out
+def denormalize_cv_array(cv_arr):
+    img = cv_arr * (255. / 2.)
+    img += 0.5 * 255.
+    return img
 
 
-def encode_img(image, mean=(0.5, 0.5, 0.5), std=(1.0, 1.0, 1.0)):
-    norm = normalize_cv_img(image, mean=mean, std=std)
+def encode_img(image):
+    norm = normalize_cv_img(image)
     return array_yxc2cyx(norm)
 
 
-def decode_img(tensor, mean=(0.5, 0.5, 0.5), std=(1.0, 1.0, 1.0)):
+def decode_img(tensor):
     arr = array_cyx2yxc(tensor)
-    arr = denormalize_cv_array(arr, mean=mean, std=std)
+    arr = denormalize_cv_array(arr)
     arr = np.clip(arr, 0, 255)
     return arr.astype(np.uint8)
 

train.py
@@ -1,17 +1,16 @@
+import cv2
 import torch
-from torch.nn import BCELoss
 from torch.utils.tensorboard import SummaryWriter
 import torchvision
 import argparse
 from tqdm import tqdm
+
 from models.discriminator import FaceDiscriminator
 from models.generator import FaceGenerator
 from dataset.faces_dataset import make_dataloader
 from torch.optim import Adam, SGD
-from metrics import accuracy
 from dataset.utils import decode_img, array_yxc2cyx
 from dataset.faces_dataset import FacesDataset, DEFAULT_RGB_MEAN, DEFAULT_RGB_STD, AddGaussianNoise
-
 from utils import get_readable_timestamp, get_total_elements_cnt
 from loss import discriminator_loss, generator_loss
 
@@ -33,23 +32,30 @@ def train(generator, discriminator, train_loader, optimizer_gen, optimizer_discr
             fake_imgs = generator.forward(random_descriptors)
 
             discriminator.train()
+            discriminator.requires_grad_(True)
             real_probs = discriminator.forward(real_imgs)
-            fake_probs = discriminator.forward(fake_imgs)
+            fake_probs = discriminator.forward(fake_imgs.detach())
             d_loss, loss_real, loss_fake = discriminator_loss(real_prob=real_probs, fake_prob=fake_probs)
             d_loss.backward()
             optimizer_discr.step()
 
-            generator.train()
-            g_loss = generator_loss(fake_prob=fake_probs)
-            g_loss.backward()
-            optimizer_gen.step()
-            for i in range(generator_trains_per_iter - 1):
-                g_loss = generator_loss(fake_prob=fake_probs)
-                g_loss.backward()
-                optimizer_gen.step()
-
             discr_real_accuracy = torch.sum(torch.where(real_probs >= 0.5, 1, 0)) / get_total_elements_cnt(real_probs)
             disc_fake_accuracy = torch.sum(torch.where(fake_probs < 0.5, 0, 1)) / get_total_elements_cnt(fake_probs)
+
+            generator.train()
+            discriminator.eval()
+            discriminator.requires_grad_(False)
+            fake_imgs = generator.forward(random_descriptors)
+            fake_probs = discriminator.forward(fake_imgs)
+            g_loss = generator_loss(fake_prob=fake_probs)
+            g_loss.backward(retain_graph=True)
+            optimizer_gen.step()
+            for i in range(generator_trains_per_iter - 1):
+                fake_imgs = generator.forward(random_descriptors)
+                fake_probs = discriminator.forward(fake_imgs)
+                g_loss = generator_loss(fake_prob=fake_probs)
+                g_loss.backward(retain_graph=True)
+                optimizer_gen.step()
 
             global_step = epoch_id * len(train_loader) + batch_idx
             if tb_writer:
@@ -71,6 +77,7 @@ def train(generator, discriminator, train_loader, optimizer_gen, optimizer_discr
                             if img.device != "cpu":
                                 img = img.cpu()
                             img = decode_img(img.detach().numpy())
+                            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                             img = array_yxc2cyx(img)
                             gen_imgs.append(torch.from_numpy(img))
 
@@ -79,16 +86,17 @@ def train(generator, discriminator, train_loader, optimizer_gen, optimizer_discr
                             if img.device != "cpu":
                                 img = img.cpu()
                             img = decode_img(img.detach().numpy())
+                            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                             img = array_yxc2cyx(img)
                             target_imgs.append(torch.from_numpy(img))
 
                         if tb_writer:
                             img_grid_gen = torchvision.utils.make_grid(gen_imgs)
                             tb_writer.add_image('Valid/Generated', img_tensor=img_grid_gen,
-                                                    global_step=global_step, dataformats='CHW')
+                                                global_step=global_step, dataformats='CHW')
                             img_grid_tgt = torchvision.utils.make_grid(target_imgs)
                             tb_writer.add_image('Valid/Real', img_tensor=img_grid_tgt,
-                                                    global_step=global_step, dataformats='CHW')
+                                                global_step=global_step, dataformats='CHW')
 
             if autosave_period is not None:
                 if (batch_idx + 1) % autosave_period == 0:
@@ -107,11 +115,11 @@ def train(generator, discriminator, train_loader, optimizer_gen, optimizer_discr
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epochs", type=int, default=1,
+    parser.add_argument("--epochs", type=int, default=2,
                         help="Number of epochs")
     parser.add_argument("--batch-train", type=int, default=32,
                         help="Size of batch for training")
-    parser.add_argument("--device", type=str, default="cuda",
+    parser.add_argument("--device", type=str, default="cpu",
                         help="Target device: cpu/cuda")
     parser.add_argument("--learning-rate", type=float, default=2e-4,
                         help="Learning rate")
@@ -133,12 +141,12 @@ def parse_args():
                         help="Use lr scheduler or not")
     parser.add_argument("--l2", type=float, default=0,
                         help="L2 reularization coefficient")
-    parser.add_argument("--noise-mean", type=float,
+    parser.add_argument("--noise-mean", type=float, default=None,
                         help="Gaussian noise mean")
-    parser.add_argument("--noise-std", type=float,
+    parser.add_argument("--noise-std", type=float, default=None,
                         help="Gaussian noise std")
     parser.add_argument("--dataset", type=str,
-                        default="/home/igor/datasets/faces",
+                        default="/home/igor/datasets/img_celeba/img_align_celeba/small",
                         help="Abs path to dataset")
     args = parser.parse_args()
     return args